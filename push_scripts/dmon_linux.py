2e4945a841fec43501d358a162e68c5b74fe81ed
Drew Streib <dtype@dtype.org>
2024-01-13 15:13:13 -0800

    limit read
    Co-authored-by: Edoardo Spadolini <edoardo.spadolini@gmail.com>

--- push_scripts/dmon_linux.py
+++ push_scripts/dmon_linux.py
@@ -88,7 +88,7 @@ def main():
                 if not args.cron:
                     # first line of body has the relevant error in our case
                     # this is just informational
-                    print(e.code, e.fp.readline().decode().strip())
+                    print(e.code, e.fp.read(16384).decode(errors="replace").strip())
             except Exception as e:
                 if not args.cron:
                     raise e from None
